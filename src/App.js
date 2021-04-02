468197bf545143a5913f604b19217f850f534798
Chatmalow <maxime.pie.mail@gmail.com>
2021-04-02 14:10:59 +0200

    refactor: Apply Atomic Design structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './style/App.scss';
-import Login from "./components/Login";
-import Welcome from "./components/Welcome";
+import Login from "./components/pages/Login";
+import Welcome from "./components/pages/Welcome";
+import Navigation from "./components/molecules/Navigation";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Êtes-vous prêt.e à faire de grandes choses en React ?</h2>
-      <Login/>
-      <Welcome/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h2>Êtes-vous prêt.e à faire de grandes choses en React ?</h2>
+        <Navigation/>
+        <Switch>
+          <Route path='/login'>
+            <Login/>
+          </Route>
+          <Route path='/welcome'>
+            <Welcome/>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
