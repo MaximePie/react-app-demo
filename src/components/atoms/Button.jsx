ab38e4832986057c72a2feda30df45b6bc1fdcb1
Chatmalow <maxime.pie.mail@gmail.com>
2021-04-02 14:32:23 +0200

    feat: Add react hooks form library

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function Button({text}) {
+export default function Button({text, onClick}) {
   return (
-    <button className="Button">
+    <button className="Button" onClick={onClick}>
       {text}
     </button>
   );
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from "../molecules/Card";
 import Button from "../atoms/Button";
+import {useForm} from "react-hook-form";
 
 export default function Login() {
+  const {register, handleSubmit, formState: {errors}} = useForm();
+
   return (
     <div className="Login">
       <h3 className="Login__header">Voulez-vous vous connecter ?</h3>
       <Card>
-        <Button text="Oui"/>
-        <Button text="Non"/>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>
+            Email
+            <input {...register("email", {required: true})}/>
+          </label>
+          {errors.email && <span>Email requis</span>}
+          <label>
+            Password
+            <input {...register("pass", {required: true})}/>
+          </label>
+          {errors.pass && <span>Password requis</span>}
+
+          <input type="submit"/>
+        </form>
+
       </Card>
     </div>
   );
+
+  function onSubmit(values) {
+    console.log("Prêts à soumettre");
+    console.log(values);
+  }
 }
